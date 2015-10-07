e7cbefb2b53ce2db67a228085572c56c8ffdf53e
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-10-07 00:26:20 -0400

    Fixed test for ensuring .local added

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -2,8 +2,8 @@ if [ -f $HOME/password-store/src/completion/pass.fish-completion ]
     . $HOME/password-store/src/completion/pass.fish-completion
 end
 
-if [ -f $HOME/.local/bin ]
-    set PATH $HOME/local/bin/ $PATH
+if test -d $HOME/.local/bin
+    set PATH $HOME/.local/bin/ $PATH
 end
 
 function start_ssh
