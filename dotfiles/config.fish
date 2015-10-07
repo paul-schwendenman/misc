dc72fe60a2e0f89f084464d82695e6773b1c455c
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-10-07 00:17:04 -0400

    Added the start_ssh helper to fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -5,3 +5,12 @@ end
 if [ -f $HOME/.local/bin ]
     set PATH $HOME/local/bin/ $PATH
 end
+
+function start_ssh
+    if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]
+        eval (ssh-agent)
+        echo 'SSH_AUTH_SOCK='"$SSH_AUTH_SOCK"
+        trap "kill $SSH_AGENT_PID" 0
+    end
+    ssh-add ~/.ssh/server_rsa
+end
