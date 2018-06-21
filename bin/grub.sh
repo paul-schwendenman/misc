f0ed6c1333486ed9b723c14913ee1f9f0e97a9ed
Paul Schwendenman <schwendenman.paul@gmail.com>
2018-06-20 23:27:27 -0400

    Remove bzr2git grub.sh ipforward minify and webcam.sh scripts

--- bin/grub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-nano -w /etc/default/grub
-grub-install /dev/sda
-grub-install --recheck /dev/sda
-
-yes | sudo apt-get install --reinstall libdebian-installer4
-sudo os-prober
-sudo update-grub
